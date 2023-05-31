44a300b6accf19b545cfdd1873772915f2399f1a
Rayhan <rayhan.alam@digital.homeoffice.gov.uk>
2023-05-31 17:32:39 +0100

    initial commit - covering basics

---
+++ Beginner-Python-Programming/basic-operators-inputs.py
@@ -0,0 +1,20 @@
+
+# print("hello, what is your name? ")
+
+# name = input()
+# print("Hello", name, "How are you today? ")
+
+# + - / * basic operators in python
+# ** exponent variable raised to the power of ( so 45**4 means 45 to the power of 4)
+# Integer division // 64 / 10 = 6.4 but with 64 // 10=6 no remainder
+# % modulus sign percentage gives us the remainder 5 % 2 = 1 (5/ 2 = 2 and with remainder of 1 no decimal point)
+
+print("Pick a Number: ")
+num1 = int(input())
+print("Pick another Number: ")
+num2 = int(input())
+answer = num1 + num2
+
+#print(type(answer))
+print(answer)
+
